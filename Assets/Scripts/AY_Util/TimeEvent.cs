eace3a934b68a6ab9877e7ae990a31af7c78628c
Atsushi Yamashita <Atsushi Yamashita>
2017-10-12 20:17:50 +0900

    Fix start time bug.

--- Assets/Scripts/AY_Util/TimeEvent.cs
+++ Assets/Scripts/AY_Util/TimeEvent.cs
@@ -1,78 +1,111 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 
-/// <summary>
-/// Use this class when you need timer event.
-/// And you should set action implemented instance to mActions.
-/// </summary>
-public class TimeEvent : MonoBehaviour
+namespace AY_Util
 {
     /// <summary>
-    /// Event array size.
+    /// Use this class when you need timer event.
+    /// And you should set action implemented instance to mActions.
     /// </summary>
-    [SerializeField]
-    private const int SIZE = 1;
-
-    /// <summary>
-    /// When this event fired, then call these events.
-    /// </summary>
-    [SerializeField]
-    private TimeEvent[] mActions = new TimeEvent[SIZE];
-
-    /// <summary>
-    /// Event execution time.
-    /// If you want not to do action, then this set under 0.
-    /// </summary>
-    [SerializeField]
-    private float mEventFireTime = -1;
-
-    /// <summary>
-    /// You can change count stat time.
-    /// </summary>
-    private float mStart = 0;
-
-    /// <summary>
-    /// This is Action event processes.
-    /// </summary>
-    /// <param name="passed"></param>
-    protected virtual void TimeAction ( float passed )
+    public class TimeEvent : MonoBehaviour
     {
-        string msg = "Call time action ({0})" + passed;
-        Log.DebugThrow( msg );
-    }
+        /// <summary>
+        /// Event array size.
+        /// </summary>
+        [SerializeField]
+        private const int SIZE = 1;
 
-    /// <summary>
-    /// When debug build, then set debug function.
-    /// </summary>
-    private void AddDebugFire ( )
-    {
-        if (!Debug.isDebugBuild) return;
-        TimeEvent[] temp = new TimeEvent[SIZE + 1];
-        temp[0] = this;
-        for (int i = 0; i < SIZE; i++) temp[i + 1] = mActions[i];
-        mActions = temp;
-    }
+        /// <summary>
+        /// When this event fired, then call these events.
+        /// </summary>
+        [SerializeField]
+        private TimeEvent[] mActions = new TimeEvent[SIZE];
 
-    /// <summary>
-    /// Setup start data.
-    /// </summary>
-    private void Start ( )
-    {
-        AddDebugFire();
-    }
+        /// <summary>
+        /// Event execution time.
+        /// If you want not to do action, then this set under 0.
+        /// </summary>
+        [SerializeField]
+        private float mEventFireTime = -1;
 
-    /// <summary>
-    /// If time passed fire time, then call all of time action.
-    /// </summary>
-    private void Update ( )
-    {
-        Assert.IsTrue( mEventFireTime > 0 );
-        float delta = Time.time - mStart;
-        if (delta < mEventFireTime) return;
-        foreach (TimeEvent action in mActions)
+        /// <summary>
+        /// When event fired, then update this time.
+        /// </summary>
+        [SerializeField]
+        private float mRepeaterTime = -1;
+
+        /// <summary>
+        /// You can change count stat time.
+        /// </summary>
+        private float mStart = 0;
+
+        /// <summary>
+        /// This is Action event processes.
+        /// </summary>
+        /// <param name="passed"></param>
+        protected virtual void TimeAction ( float passed )
         {
-            if (action == null) continue;
-            action.TimeAction( delta );
+            string msg = "Call time action ({0})" + passed;
+            Log.DebugThrow( msg );
+        }
+
+        /// <summary>
+        /// Set to fire time.
+        /// </summary>
+        /// <param name="fire">new fire time.</param>
+        protected void SetFireTime(float fire )
+        {
+            mEventFireTime = fire;
+        }
+
+        /// <summary>
+        /// Set repeat time.
+        /// If you want to end of repeat, then you should set under 0.
+        /// </summary>
+        /// <param name="repeater">repeat time.</param>
+        protected void SetRepeaterTime(float repeater)
+        {
+            mRepeaterTime = repeater;
+        }
+
+        /// <summary>
+        /// When debug build, then set debug function.
+        /// </summary>
+        private void AddDebugFire ( )
+        {
+            if (!Debug.isDebugBuild) return;
+            TimeEvent[] temp = new TimeEvent[SIZE + 1];
+            temp[0] = this;
+            for (int i = 0; i < SIZE; i++) temp[i + 1] = mActions[i];
+            mActions = temp;
+
+        }
+
+        /// <summary>
+        /// Setup start data.
+        /// Add debug fire and start time set.
+        /// </summary>
+        private void Start ( )
+        {
+            AddDebugFire();
+            mStart = Time.time;
+        }
+
+        /// <summary>
+        /// If time passed fire time, then call all of time action.
+        /// </summary>
+        private void Update ( )
+        {
+            if( mEventFireTime <= 0 ) return;
+            float delta = Time.time - mStart;
+            if (delta < mEventFireTime) return;
+            foreach (TimeEvent action in mActions)
+            {
+                if (action == null) continue;
+                action.TimeAction( delta );
+            }
+            mEventFireTime = mRepeaterTime;
+            mStart = Time.time;
         }
     }
 }
