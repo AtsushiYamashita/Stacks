5441cd885afd6e45702499520fded156ff9128ea
Atsushi Yamashita <Atsushi Yamashita>
2017-10-12 18:36:39 +0900

    Add scene move.

---
+++ Assets/Scripts/AY_Util/SceneMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Scene move action component.
+/// </summary>
+public class SceneMove : MonoBehaviour
+{
+    /// <summary>
+    /// This value express next scene name.
+    /// </summary>
+    [SerializeField]
+    private string mMoveTo = "SceneName";
+
+    /// <summary>
+    /// This is process of scene move.
+    /// </summary>
+    public void Execute ( )
+    {
+        Scene isScene = SceneManager.GetSceneByName( mMoveTo );
+        if (!isScene.IsValid())
+        {
+            SceneManager.LoadScene( mMoveTo );
+        }
+
+        string errorMSG = "Scene move can read next scene. You should check scene name. ";
+        throw new System.Exception( errorMSG );
+    }
+}
